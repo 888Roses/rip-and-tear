6eda39170de3efa0163b186df6722dba64fa828a
Rose <skillwarzaking@gmail.com>
2025-05-26 15:45:12 +0200

    🔥 Changed for loop for Mathf.inLine in ThrownWarperItem.

--- src/main/java/net/rose/rip_and_tear/common/item/ThrownWarperItem.java
+++ src/main/java/net/rose/rip_and_tear/common/item/ThrownWarperItem.java
@@ -1,7 +1,9 @@
 package net.rose.rip_and_tear.common.item;
 
 import net.rose.rip_and_tear.common.entity.projectile.WarperProjectileEntity;
+
 import static net.rose.rip_and_tear.common.init.ModConfiguration.*;
+
 import net.rose.rip_and_tear.common.init.ModParticleTypes;
 import net.rose.rip_and_tear.common.init.ModComponents;
 import net.rose.rip_and_tear.common.init.ModItems;
@@ -33,56 +35,58 @@ public class ThrownWarperItem extends Item {
         return (WarperProjectileEntity) serverWorld.getEntity(uuid);
     }
 
-    protected void createTrail(ServerWorld serverWorld, WarperProjectileEntity warperProjectileEntity, PlayerEntity user) {
+    protected void createTrail(ServerWorld serverWorld, WarperProjectileEntity warperProjectileEntity,
+                               PlayerEntity user) {
         var startingPosition = warperProjectileEntity.getPos();
         var destinationPosition = user.getPos().add(0, 0.25F, 0);
-        var distance = destinationPosition.distanceTo(startingPosition);
-        var direction = destinationPosition.subtract(startingPosition).normalize();
 
-        for (var i = 0F; i < distance / THROWN_WARPER_RECALL_TRAIL_INTERVAL; i += THROWN_WARPER_RECALL_STEP_SIZE) {
-            var stepPosition = startingPosition.add(direction.multiply(i));
-            var stepBlockPosition = serverWorld.getTopPosition(
-                    Heightmap.Type.MOTION_BLOCKING_NO_LEAVES,
-                    BlockPos.ofFloored(stepPosition)
-            );
+        Mathf.inLine(
+                startingPosition, destinationPosition,
+                THROWN_WARPER_RECALL_TRAIL_INTERVAL, THROWN_WARPER_RECALL_STEP_SIZE,
+                stepPosition -> {
+                    var stepBlockPosition = serverWorld.getTopPosition(
+                            Heightmap.Type.MOTION_BLOCKING_NO_LEAVES,
+                            BlockPos.ofFloored(stepPosition)
+                    );
 
-            // region Spawn particles on the way.
+                    // region Spawn particles on the way.
 
-            serverWorld.spawnParticles(
-                    ModParticleTypes.CHARTER_SKULL,
-                    stepPosition.x, stepBlockPosition.getY() + 0.5F, stepPosition.z, 1,
-                    THROWN_WARPER_RECALL_PARTICLE_SPREAD,
-                    THROWN_WARPER_RECALL_PARTICLE_SPREAD,
-                    THROWN_WARPER_RECALL_PARTICLE_SPREAD,
-                    0
-            );
+                    serverWorld.spawnParticles(
+                            ModParticleTypes.CHARTER_SKULL,
+                            stepPosition.x, stepBlockPosition.getY() + 0.5F, stepPosition.z, 1,
+                            THROWN_WARPER_RECALL_PARTICLE_SPREAD,
+                            THROWN_WARPER_RECALL_PARTICLE_SPREAD,
+                            THROWN_WARPER_RECALL_PARTICLE_SPREAD,
+                            0
+                    );
 
-            // endregion
+                    // endregion
 
-            // region Hurt every entity around each position.
+                    // region Hurt every entity around each position.
 
-            var entityHurtBox = new Box(stepBlockPosition).expand(
-                    THROWN_WARPER_RECALL_DAMAGE_RADIUS, 0,
-                    THROWN_WARPER_RECALL_DAMAGE_RADIUS
-            );
-            var damageSource = warperProjectileEntity.getDamageSource(serverWorld);
-            serverWorld.getEntitiesByType(
-                    TypeFilter.instanceOf(Entity.class), entityHurtBox,
-                    x -> {
-                        var hasDamaged = x.damage(serverWorld, damageSource, THROWN_WARPER_RECALL_DAMAGE);
+                    var entityHurtBox = new Box(stepBlockPosition).expand(
+                            THROWN_WARPER_RECALL_DAMAGE_RADIUS, 0,
+                            THROWN_WARPER_RECALL_DAMAGE_RADIUS
+                    );
+                    var damageSource = warperProjectileEntity.getDamageSource(serverWorld);
+                    serverWorld.getEntitiesByType(
+                            TypeFilter.instanceOf(Entity.class), entityHurtBox,
+                            x -> {
+                                var hasDamaged = x.damage(serverWorld, damageSource, THROWN_WARPER_RECALL_DAMAGE);
 
-                        SoundUtil.playSound(
-                                serverWorld, null, x.getPos(),
-                                ModSoundEvents.WARPER_SWING, user.getSoundCategory(),
-                                WARPER_SWING_SOUND_VOLUME, Mathf.random(serverWorld, 0.9F, 1.1F)
-                        );
+                                SoundUtil.playSound(
+                                        serverWorld, null, x.getPos(),
+                                        ModSoundEvents.WARPER_SWING, user.getSoundCategory(),
+                                        WARPER_SWING_SOUND_VOLUME, Mathf.random(serverWorld, 0.9F, 1.1F)
+                                );
 
-                        return hasDamaged;
-                    }
-            );
+                                return hasDamaged;
+                            }
+                    );
 
-            // endregion
-        }
+                    // endregion
+                }
+        );
     }
 
     @Override
