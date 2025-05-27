53533111548f528171992015010ffd6d1bc07f2f
Rose <skillwarzaking@gmail.com>
2025-05-27 11:13:07 +0200

    🎇 Added base classes for adding blocks and block entities.

---
+++ src/main/java/net/rose/rip_and_tear/common/init/ModBlockEntityTypes.java
@@ -0,0 +1,7 @@
+package net.rose.rip_and_tear.common.init;
+
+public class ModBlockEntityTypes {
+
+    public static void init() {
+    }
+}
---
+++ src/main/java/net/rose/rip_and_tear/common/init/ModBlocks.java
@@ -0,0 +1,6 @@
+package net.rose.rip_and_tear.common.init;
+
+public class ModBlocks {
+
+    public static void init() {}
+}
