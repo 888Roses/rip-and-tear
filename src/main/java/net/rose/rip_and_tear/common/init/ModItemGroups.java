3d850de5f0fb698208455f2b0df0746081a260d4
Rose <skillwarzaking@gmail.com>
2025-05-30 17:07:55 +0200

    ✨ Renamed the RUNE_MOUTHPIECE to ORANGE_RUNE.

--- src/main/java/net/rose/rip_and_tear/common/init/ModItemGroups.java
+++ src/main/java/net/rose/rip_and_tear/common/init/ModItemGroups.java
@@ -2,11 +2,8 @@ package net.rose.rip_and_tear.common.init;
 
 import moriyashiine.strawberrylib.api.module.SLibRegistries;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemGroups;
 import net.minecraft.item.ItemStack;
-import net.minecraft.registry.RegistryKeys;
 import net.rose.rip_and_tear.common.util.TextUtils;
 
 public class ModItemGroups {
@@ -19,10 +16,10 @@ public class ModItemGroups {
                         entries.add(ModItems.FATE_CRUSHER);
                         entries.add(ModItems.DUSKS_EPITATH_SCULPTURE);
                         entries.add(ModItems.RUNE_BLANK);
-                        entries.add(ModItems.RUNE_MOUTHPIECE);
+                        entries.add(ModItems.ORANGE_RUNE);
                         entries.add(ModItems.STATUE);
                     })
-                    .icon(() -> new ItemStack(ModItems.RUNE_MOUTHPIECE))
+                    .icon(() -> new ItemStack(ModItems.ORANGE_RUNE))
                     .special()
                     .build()
     );
